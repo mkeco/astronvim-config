571273169f4e0c4bade163785ddcc2b0a05a5421
mkzero <mkzeroagem@gmail.com>
2023-12-31 19:51:46 +0800

    fix neovide

--- options.lua
+++ options.lua
@@ -7,6 +7,8 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    -- neovide font
+    guifont = "MesloLGM Nerd Font Mono:h24",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
@@ -17,6 +19,33 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    
+    neovide_transparency = 0.0,
+    neovide_confirm_quit = true,
+    transparency = 0.8,
+    neovide_background_color = "#0f1117", 
+
+    neovide_remember_window_size = true,
+
+    neovide_floating_blur_amount_x = 2.0,
+    neovide_floating_blur_amount_y = 2.0,
+
+    neovide_floating_shadow = true,
+    neovide_floating_z_height = 10,
+    neovide_light_angle_degrees = 45,
+    neovide_light_radius = 5,
+
+
+    -- only mac true
+
+    neovide_input_macos_alt_is_meta = true,
+
+    neovide_refresh_rate = 60,
+
+    -- Cursor Settings
+    neovide_cursor_animate_in_insert_mode = true,
+
+    neovide_cursor_animate_command_line = true,
   },
 }
 -- If you need more control, you can use the function()...end notation
